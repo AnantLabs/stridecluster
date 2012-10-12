1592a138efbfe98f6cf39a09a6e4b03243fe8cd3
mxl.neverstop@gmail.com <mxl.neverstop@gmail.com@22fa6ea4-4967-c857-4923-4a8d170d2194>
2012-10-12 09:58:51 +0000

    添加了一些注释.

--- strideCluster/src/main/java/com/lin/stride/search/request/NovelHit.java
+++ strideCluster/src/main/java/com/lin/stride/search/request/NovelHit.java
@@ -6,26 +6,29 @@ import java.io.IOException;
 
 import com.lin.stride.utils.EfficientWritable;
 
-public class NovelHit implements EfficientWritable{
-	
+public class NovelHit implements EfficientWritable {
+
 	private String name;
-	
+
 	public NovelHit() {
 	}
+
 	@Override
 	public void readDate(DataInput in) throws IOException {
 		name = in.readUTF();
 	}
-	
+
 	@Override
 	public void writeData(DataOutput out) throws IOException {
 		out.writeUTF(name);
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 }
