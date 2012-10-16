dfa5c20bbe3b13d3b436f0b517b0741cf6566d62
mxl.neverstop@gmail.com <mxl.neverstop@gmail.com@22fa6ea4-4967-c857-4923-4a8d170d2194>
2012-10-16 11:20:27 +0000

    更新可能排队导致索引被删除

--- strideCluster/src/main/java/com/lin/stride/hdfs/HDFSFileSystem.java
+++ strideCluster/src/main/java/com/lin/stride/hdfs/HDFSFileSystem.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
@@ -27,12 +28,17 @@ import com.lin.stride.utils.ConfigReader;
  */
 public final class HDFSFileSystem {
 
-	private Configuration conf = new Configuration();
-	private FileSystem hdfs = null;
-	private Logger LOG = Logger.getLogger(HDFSFileSystem.class);
+	private final Configuration conf = new Configuration();
+	private FileSystem hdfs;
+	private final Logger LOG = Logger.getLogger(HDFSFileSystem.class);
 	private static final int BUFFERED_SIZE = 1024 * 4;
-	private String hdfsRoot = ConfigReader.getEntry("hadoop_index_path");
-	private String localIndexRoot = ConfigReader.getEntry("indexStorageDir");
+	private final String hdfsRoot = ConfigReader.getEntry("hadoop_index_path");
+	private final String localIndexRoot = ConfigReader.getEntry("indexStorageDir");
+	/* add 2012-10-16 xiaolin 更新时可能出现排队,上次所有集群还有机器没有更新下载完,这时又有新的....
+	 * 不对,跑在2个jvm中,这个锁没有,写的锁是有作用的,读的锁没必要加上.只能通过zk中的状态来判断是否所有机器
+	 * 都更新完数据,如果这时有上传索引的 ,就会导致更新索引的server异常,读取的文件不存在了,被删除了.
+	 * */
+	private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
 	public HDFSFileSystem() {
 		try {
@@ -47,31 +53,39 @@ public final class HDFSFileSystem {
 	 * Date : 2012-9-25
 	 * @throws IOException
 	 */
-	public void upLoadIndex() throws IOException {
-		FileStatus[] FileStatus = hdfs.listStatus(new Path(hdfsRoot));
-		for (FileStatus fs : FileStatus) {
-			hdfs.delete(fs.getPath(), true);
-		}
-		File indexPath = new File(ConfigReader.getEntry("indexStorageDir"));
-		File[] indexFiles = indexPath.listFiles();
-		FileInputStream fis;
-		FSDataOutputStream fsdos;
-		for (File indexFile : indexFiles) {
-			fis = new FileInputStream(indexFile);
-			Path hdfsFile = new Path(hdfsRoot, indexFile.getName());
-			fsdos = hdfs.create(hdfsFile);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			BufferedOutputStream bos = new BufferedOutputStream(fsdos);
-			byte[] b = new byte[BUFFERED_SIZE];
-			int n = 0;
-			while ((n = bis.read(b)) != -1) {
-				bos.write(b, 0, n);
+	public void upLoadIndex() {
+		FileStatus[] FileStatus;
+		lock.writeLock().lock();
+		try {
+			FileStatus = hdfs.listStatus(new Path(hdfsRoot));
+			for (FileStatus fs : FileStatus) {
+				hdfs.delete(fs.getPath(), true);
 			}
-			LOG.info("upLoad ... " + FileUtils.byteCountToDisplaySize(indexFile.length()) + "\t" + hdfsFile.toString());
-			bos.close();
-			bis.close();
-			fsdos.close();
-			fis.close();
+			File indexPath = new File(ConfigReader.getEntry("indexStorageDir"));
+			File[] indexFiles = indexPath.listFiles();
+			FileInputStream fis;
+			FSDataOutputStream fsdos;
+			for (File indexFile : indexFiles) {
+				fis = new FileInputStream(indexFile);
+				Path hdfsFile = new Path(hdfsRoot, indexFile.getName());
+				fsdos = hdfs.create(hdfsFile);
+				BufferedInputStream bis = new BufferedInputStream(fis);
+				BufferedOutputStream bos = new BufferedOutputStream(fsdos);
+				byte[] b = new byte[BUFFERED_SIZE];
+				int n = 0;
+				while ((n = bis.read(b)) != -1) {
+					bos.write(b, 0, n);
+				}
+				LOG.info("upLoad ... " + FileUtils.byteCountToDisplaySize(indexFile.length()) + "\t" + hdfsFile.toString());
+				bos.close();
+				bis.close();
+				fsdos.close();
+				fis.close();
+			}
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+		} finally {
+			lock.writeLock().unlock();
 		}
 	}
 
@@ -80,28 +94,35 @@ public final class HDFSFileSystem {
 	 * Date : 2012-9-25
 	 * @throws IOException
 	 */
-	public void downLoadIndex() throws IOException {
-		FileStatus[] FileStatus = hdfs.listStatus(new Path(hdfsRoot));
-		FSDataInputStream fsis;
-		FileOutputStream fos;
-		for (FileStatus fs : FileStatus) {
-			File localFile = new File(localIndexRoot, fs.getPath().getName());
-			fos = new FileOutputStream(localFile);
-			fsis = hdfs.open(fs.getPath());
-			BufferedInputStream bis = new BufferedInputStream(fsis);
-			BufferedOutputStream bos = new BufferedOutputStream(fos);
+	public void downLoadIndex() {
+		lock.readLock().lock();
+		try {
+			FileStatus[] FileStatus = hdfs.listStatus(new Path(hdfsRoot));
+			FSDataInputStream fsis;
+			FileOutputStream fos;
+			for (FileStatus fs : FileStatus) {
+				File localFile = new File(localIndexRoot, fs.getPath().getName());
+				fos = new FileOutputStream(localFile);
+				fsis = hdfs.open(fs.getPath());
+				BufferedInputStream bis = new BufferedInputStream(fsis);
+				BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-			byte[] b = new byte[BUFFERED_SIZE];
+				byte[] b = new byte[BUFFERED_SIZE];
 
-			int n = 0;
-			while ((n = bis.read(b)) != -1) {
-				bos.write(b, 0, n);
+				int n = 0;
+				while ((n = bis.read(b)) != -1) {
+					bos.write(b, 0, n);
+				}
+				LOG.info("download ... " + FileUtils.byteCountToDisplaySize(fs.getLen()) + "\t" + localFile.getAbsolutePath());
+				bos.close();
+				bis.close();
+				fos.close();
+				fsis.close();
 			}
-			LOG.info("download ... " + FileUtils.byteCountToDisplaySize(fs.getLen()) + "\t" + localFile.getAbsolutePath());
-			bos.close();
-			bis.close();
-			fos.close();
-			fsis.close();
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+		} finally {
+			lock.readLock().unlock();
 		}
 	}
 
